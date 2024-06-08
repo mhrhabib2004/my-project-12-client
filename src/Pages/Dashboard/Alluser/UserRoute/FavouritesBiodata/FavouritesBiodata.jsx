7693ee2df16007e77db08f8bc094cb5ad1177714
mhrhabib2004 <mhrhabib2004@gmail.com>
2024-06-09 04:11:56 +0600

    added delete button and table data

--- src/Pages/Dashboard/Alluser/UserRoute/FavouritesBiodata/FavouritesBiodata.jsx
+++ src/Pages/Dashboard/Alluser/UserRoute/FavouritesBiodata/FavouritesBiodata.jsx
@@ -6,13 +6,40 @@ const FavouritesBiodata = () => {
     // console.log(favorites)
     
     return (
-       <div>
-         {
-            favorites.map(favorite=><FavoritesDataCard key={favorite._id} favorite={favorite}></FavoritesDataCard>)
-        }
-    
+        <div>
+            
+   <div className="flex flex-col mt-6">
+     <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+       <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+         <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
+           <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+            
+             <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
+               
+                 <div>
+        
+        {
+           favorites.map(favorite=><FavoritesDataCard key={favorite._id} favorite={favorite}></FavoritesDataCard>)
+       }
+   
+      </div>
+               {/* Repeat similar structure for other team members */}
+             </tbody>
+           </table>
+         </div>
        </div>
+     </div>
+   </div>
+     </div>
     );
 };
 
 export default FavouritesBiodata;
+
+{/* <div>
+        
+        {
+           favorites.map(favorite=><FavoritesDataCard key={favorite._id} favorite={favorite}></FavoritesDataCard>)
+       }
+   
+      </div> */}

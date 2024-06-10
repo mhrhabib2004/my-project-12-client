c3a86733e8876a7a73c7fb44b2fb32051fe0fe25
mhrhabib2004 <mhrhabib2004@gmail.com>
2024-06-10 22:48:18 +0600

    added jwt

--- src/Provaiders/AuthProvider.jsx
+++ src/Provaiders/AuthProvider.jsx
@@ -1,6 +1,8 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from "../Firebase/firebase.config";
+import axios from "axios";
+import useAxiosPublic from "../Hooks/useAxiosPublic";
 
 
 
@@ -12,7 +14,9 @@ const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const googleProvider = new GoogleAuthProvider();
+    const axiosPublic = useAxiosPublic();
     const [loading, setLoading] = useState(true);
+
 
     const createUser = (email, password) => {
         setLoading(true);
@@ -43,13 +47,27 @@ const AuthProvider = ({ children }) => {
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
-            console.log('current user', currentUser);
+            if (currentUser) {
+                // get token and store client
+                const userInfo = { email: currentUser.email };
+                axiosPublic.post('/jwt', userInfo)
+                    .then(res => {
+                        if (res.data.token) {
+                            localStorage.setItem('access-token', res.data.token);
+                        }
+                    })
+            }
+            else {
+                // TODO: remove token (if token stored in the client side: Local storage, caching, in memory)
+                localStorage.removeItem('access-token');
+            }
             setLoading(false);
         });
         return () => {
             return unsubscribe();
         }
-    }, [])
+    }, [axiosPublic])
+
 
     const authInfo = {
         user,
